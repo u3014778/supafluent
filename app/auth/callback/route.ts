05752b62eecdd0ddcfc4f443e7cdbcc9e6105816
u3014778 <33469959+u3014778@users.noreply.github.com>
2024-10-01 21:41:10 +1000

    fix build error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
-
+import { cookies } from 'next/headers';
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
@@ -10,7 +10,8 @@ export async function GET(request: Request) {
   const origin = requestUrl.origin;
 
   if (code) {
-    const supabase = createClient();
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
     await supabase.auth.exchangeCodeForSession(code);
   }
 
